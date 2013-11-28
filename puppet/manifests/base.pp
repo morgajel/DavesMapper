1ccbf6085059c2d02a4320126abb1290a581223b
Jesse Morgan <morgajel@gmail.com>
2013-11-28 13:33:42 -0500

    exec to fill DB

--- puppet/manifests/base.pp
+++ puppet/manifests/base.pp
@@ -15,6 +15,7 @@ node default {
         max_user_connections     => '0',
         password_hash            => '*7ACE763ED393514FE0C162B93996ECD195FFC4F5',  # "changeme" was created via SELECT PASSWORD('changeme');
         #require =>Class['::mysql::client','::mysql::server']
+        require=>Class['mysql::server'],
       }
     
     mysql_grant{ 'dmap@localhost/dmillar_cartography.*' : 
@@ -24,13 +25,22 @@ node default {
         table      => 'dmillar_cartography.*',
         user       => 'dmap@localhost',
         #require =>Class['::mysql::client','::mysql::server']
+        require=>Class['mysql::server'],
     }
     mysql_database{  'dmillar_cartography':
         ensure  => 'present',
         charset => 'utf8',
         #require =>Class['::mysql::client','::mysql::server']
+        require=>Class['mysql::server'],
+        notify => Exec['load db'],
     }
-
+    exec{ 'load db':
+        command => 'mysql dmillar_cartography < /vagrant/db/dmillar_cartography.sql',
+        refreshonly => true,
+        require=>[Class['mysql::server'],Mysql_database['dmillar_cartography'],Mysql_grant['dmap@localhost/dmillar_cartography.*'],Mysql_user['dmap@localhost'] ],
+        path =>['/usr/bin'],
+    
+    }
 
 }
 
